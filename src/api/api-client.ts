6ff2688ba556ed2b2817e3f31a82a65a9a41fd7d
DomZem <dominik.zemlik.dev@gmail.com>
2025-01-08 19:50:15 +0100

    feat: updated api client

--- src/api/api-client.ts
+++ src/api/api-client.ts
@@ -1,10 +1,10 @@
 import { dateReviver } from '@/helpers/json-helper';
-import { ApiClientBase, type LoginResult } from './api-client-base';
-// import { Dialog, Loading } from 'quasar'
+import { ApiClientBase, type DeviceRegisterDTO, type TokenResult } from './api-client-base';
 
 export class ApiClient extends ApiClientBase {
-	#authToken?: string;
-	#authTokenExpires?: Date;
+	userTokenResult?: TokenResult;
+	deviceTokenResult?: TokenResult;
+	onUnauthorized?: (url: string) => void;
 
 	constructor(url: string) {
 		const http: unknown = {
@@ -13,28 +13,53 @@ export class ApiClient extends ApiClientBase {
 		super(url, http as ApiHttpClient);
 		(http as ApiHttpClient).fetch = this.fetchWrapper.bind(this);
 
-		const token = localStorage.getItem('jwt');
-		if (token) {
-			this.#authToken = token;
-		}
-
 		this.jsonParseReviver = dateReviver;
+
+		// restore auth when present in storage:
+		this.restoreTokens();
 	}
 
-	override userLogin(loginName: string, password: string): Promise<LoginResult> {
+	/**
+	 * Restores token from storage
+	 */
+	restoreTokens() {
+		const rawDeviceAuthData = window.localStorage.getItem('attend-me:deviceAuthData');
+		if (rawDeviceAuthData) {
+			this.deviceTokenResult = JSON.parse(rawDeviceAuthData, dateReviver);
+		}
+
+		const rawUserAuthData = window.sessionStorage.getItem('attend-me:userAuthData');
+		if (rawUserAuthData) {
+			this.userTokenResult = JSON.parse(rawUserAuthData, dateReviver);
+		}
+	}
+
+	override userLogin(loginName: string, password: string): Promise<TokenResult> {
 		return super.userLogin(loginName, password).then((r) => {
 			if (!r || !r.token) throw new Error('Authorization response returned empty token!');
 
 			// set token for further processing
-			this.#authToken = r.token;
-			this.#authTokenExpires = r.expires;
+			this.userTokenResult = r;
+
+			//
+			window.sessionStorage.setItem('attend-me:userAuthData', JSON.stringify(r));
 
 			return r;
 		});
 	}
 
-	isTokenExpired() {
-		return !this.#authTokenExpires || this.#authTokenExpires < new Date();
+	userDeviceRegisterWithToken(token: string, data: DeviceRegisterDTO): Promise<TokenResult> {
+		this.deviceTokenResult = {
+			token,
+		};
+		return super.userDeviceRegister(data).then((r) => {
+			this.deviceTokenResult = r;
+
+			// store device token
+			window.localStorage.setItem('attend-me:deviceAuthData', JSON.stringify(r));
+
+			return r;
+		});
 	}
 
 	private fetchWrapper(requestInfo: RequestInfo, init?: RequestInit): Promise<Response> {
@@ -44,15 +69,40 @@ export class ApiClient extends ApiClientBase {
 
 		const requestUrl = typeof requestInfo === 'string' ? requestInfo : requestInfo.url;
 
-		if (this.#authToken && !requestUrl.startsWith('/user/login')) {
+		const parsedUrl = new URL(requestUrl);
+		const path = parsedUrl.pathname;
+		if (
+			this.deviceTokenResult &&
+			(path.startsWith('/user/device/register') || path.startsWith('/user/attendance/ticket/get') || path.startsWith('/course/session/attendance/register')) &&
+			!path.startsWith('/user/device/register/token/get')
+		) {
 			opts.headers = {
 				...opts.headers,
-				Authorization: `Bearer ${this.#authToken}`,
+				Authorization: `Bearer ${this.deviceTokenResult.token}`,
+			};
+		} else if (this.userTokenResult && !path.startsWith('/user/login')) {
+			opts.headers = {
+				...opts.headers,
+				Authorization: `Bearer ${this.userTokenResult.token}`,
 			};
 		}
 
-		return fetch(requestInfo, opts);
+		const request = fetch(requestInfo, opts);
+
+		if (this.onUnauthorized) {
+			return request.then((response) => {
+				if (response.status === 401 && this.onUnauthorized) {
+					this.onUnauthorized(requestUrl);
+				}
+
+				return response;
+			});
+		}
+
+		return request;
 	}
+
+	private restore() {}
 }
 
 type ApiHttpClient = {
